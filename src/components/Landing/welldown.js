ec4cf1f7b99133964457157b30555bf73a344421
Smart-Dev719 <blockchaindev719@gmail.com>
2022-04-05 16:21:16 -0400

    match

--- src/components/Landing/welldown.js
+++ src/components/Landing/welldown.js
@@ -28,7 +28,7 @@ const Welldown = () => {
                         viewport={{ once: true, amount: 0 }}
                         className="text-white fs-20 font-origin"
                     >
-                        When you buy a Real Boy Bot you're not just buying an avatar. You are gaining membership access to a club like no other. Our long term goal is to open the very first virtual tattoo shop within the metaverse. We believe all avatars should look sick, most are already flexing digital kicks and clothing, So shouldn't they also have the tattoos to match
+                        When you buy a Real Boy Bot you're not just buying an avatar. You are gaining membership access to a club like no other. Our long term goal is to open the very first virtual tattoo shop within the metaverse. We believe all avatars should look sick, most are already flexing digital kicks and clothing, So shouldn't they also have the tattoos to match?
                         Shouldn’t these tattoos be your designs? There is no NFT project that gives back to their community or creates opportunity like we do.
                     </motion.div>
                 </div>

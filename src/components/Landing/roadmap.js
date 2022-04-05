ed4c64a637b746f2fa8928f23c0c2670606b96f6
Smart-Dev719 <blockchaindev719@gmail.com>
2022-04-05 17:07:16 -0400

    comma error

--- src/components/Landing/roadmap.js
+++ src/components/Landing/roadmap.js
@@ -37,7 +37,7 @@ const Roadmap = () => {
                             <span className="font-bold">A gift from us to you</span>
                             <div className="content font-origin">
                                 <p className="para_2" onClick={readmore2}>
-                                    As promised all owners of the 8008 real boy bots will be eligible for 1 of 1000 tattoo kits. This kit will include everything you need to start practicing your tattoo skills on fake skin, just like your bot wears! Please, whatever you do, DO NOT TATTOO human skin… seriously we cant stand the sight of shit tattoos on human skin, it's not cool and it's not clever, nobody will say you look good, you may even catch a disease, so seriously, don’t do it. While learning how to tattoo stick to fake skin.
+                                    As promised all owners of the 8008 real boy bots will be eligible for 1 of 1000 tattoo kits. This kit will include everything you need to start practicing your tattoo skills on fake skin, just like your bot wears! Please, whatever you do, DO NOT TATTOO human skin... &nbsp; seriously we cant stand the sight of shit tattoos on human skin, it's not cool and it's not clever, nobody will say you look good, you may even catch a disease, so seriously, don’t do it. While learning how to tattoo stick to fake skin.
                                 </p>
                             </div>
                         </li>
@@ -54,7 +54,7 @@ const Roadmap = () => {
                             <span className="font-bold">Level up</span>
                             <div className="content font-origin">
                                 <p className="para_4" onClick={readmore4}>
-                                    V-02 bot, every owner of a V-01 real boy bot will be gifted a 3D V-02 real boy bot. These bots will be able to access the real boy bot club studio, be able to choose from our collection of tattoos and get them inked on there avatars. We will also be engaging with our community in order to create the opportunity for talented individuals to design and earn money for their creations, this hasn't been done before in the NFT space, and we know it will be an incredible opportunity for everyone. The designs we
+                                    V-02 bot, every owner of a V-01 real boy bot will be gifted a 3D V-02 real boy bot. These bots will be able to access the real boy bot club studio, be able to choose from our collection of tattoos and get them inked on their avatars. We will also be engaging with our community in order to create the opportunity for talented individuals to design and earn money for their creations, this hasn't been done before in the NFT space, and we know it will be an incredible opportunity for everyone. The designs we
                                     choose will be displayed in our metaverse studio and every time someone selects the design to be tattooed on their avatar, the creator of the design will earn a % of the purchase, and this is just the beginning.
                                 </p>
                             </div>

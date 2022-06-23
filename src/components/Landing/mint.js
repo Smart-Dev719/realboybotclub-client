f43b5eb546d642a97f49ae9090b48b3b4a2e9694
Magic0913 <magicjohnson.ca@gmail.com>
2022-06-24 07:52:16 +0900

    Add mint section and caculating price function in landing page

--- src/components/Landing/mint.js
+++ src/components/Landing/mint.js
@@ -1,36 +1,36 @@
 import React, { useState } from "react";
-import FlipCountdown from '@rumess/react-flip-countdown';
+import FlipCountdown from "@rumess/react-flip-countdown";
 import calculator from "../../assets/image/calculator.png";
-import { motion } from 'framer-motion/dist/framer-motion';
+import { motion } from "framer-motion/dist/framer-motion";
 
 const Mint = () => {
+  const initalPrice = 0.2;
   const [nft, setNft] = useState(1);
-  const [totalPrice, setTotalPrice] = useState(0);
-
+  const [totalPrice, setTotalPrice] = useState(0.2);
   const plus_nft = (e_nft) => {
     var p_nft = e_nft + 1;
     if (p_nft >= 9) {
-      setTotalPrice(0);
+      setTotalPrice((9 * initalPrice).toFixed(1));
       setNft(9);
     } else {
-      setTotalPrice(p_nft * 0)
+      setTotalPrice((p_nft * initalPrice).toFixed(1));
       setNft(p_nft);
     }
-  }
+  };
   const minus_nft = (e_nft) => {
     var p_nft = e_nft - 1;
     if (p_nft < 1) {
-      setTotalPrice(0);
+      setTotalPrice(initalPrice);
       setNft(1);
     } else {
-      setTotalPrice(p_nft * 0)
+      setTotalPrice((p_nft * initalPrice).toFixed(1));
       setNft(p_nft);
     }
-  }
+  };
   const max_nft = () => {
-    setTotalPrice(0);
+    setTotalPrice((9 * initalPrice).toFixed(1));
     setNft(9);
-  }
+  };
   return (
     <div className="row mt-5 justify-content-center">
       <div className="col-lg-7 col-cd-12 col-xs-12 d-flex flex-column justify-content-between main_mint">
@@ -41,11 +41,7 @@ const Mint = () => {
           viewport={{ once: true, amount: 0 }}
           className="bg-pink date_panel"
         >
-          <FlipCountdown
-            hideYear
-            hideMonth
-            endAt={'2022-03-22 00:00:00'}
-          />
+          <FlipCountdown hideYear hideMonth endAt={"2022-03-22 00:00:00"} />
         </motion.div>
         <motion.div
           initial={{ opacity: 0, y: 50 }}
@@ -57,19 +53,37 @@ const Mint = () => {
           <div className="mint_space"></div>
           <div className="d-flex flex-column text-white justify-content-center align-items-center price_area">
             <div>Price</div>
-            <div><span className="fs-30 text-pink totalprice">{totalPrice}</span> ETH</div>
+            <div>
+              <span className="fs-30 text-pink totalprice">{totalPrice}</span>{" "}
+              ETH
+            </div>
           </div>
           <div className="d-flex justify-content-end align-items-center minus_btn">
-            <span className="fs-20 text-white cursor-pointer" onClick={() => minus_nft(nft)}><i className="fa fa-minus" /></span>
+            <span
+              className="fs-20 text-white cursor-pointer"
+              onClick={() => minus_nft(nft)}
+            >
+              <i className="fa fa-minus" />
+            </span>
           </div>
           <div className="d-flex justify-content-center align-items-center flex-column cnt">
             <input type="text" value={nft} disabled />
           </div>
           <div className="d-flex align-items-center plus_btn">
-            <span className="fs-20 text-white cursor-pointer" onClick={() => plus_nft(nft)}><i className="fa fa-plus" /></span>
+            <span
+              className="fs-20 text-white cursor-pointer"
+              onClick={() => plus_nft(nft)}
+            >
+              <i className="fa fa-plus" />
+            </span>
           </div>
           <div className="d-flex align-items-center justify-content-center max_area">
-            <span className="d-flex align-items-center justify-content-center fs-20 cursor-pointer max_btn" onClick={max_nft}>max</span>
+            <span
+              className="d-flex align-items-center justify-content-center fs-20 cursor-pointer max_btn"
+              onClick={max_nft}
+            >
+              max
+            </span>
           </div>
           <div className="mint_space"></div>
         </motion.div>

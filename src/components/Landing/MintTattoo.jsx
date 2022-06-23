7293a77f0e4b9303ff9d64fc01deb332ee6c528e
Magic0913 <magicjohnson.ca@gmail.com>
2022-06-24 00:44:58 +0900

    redesign mint page

---
+++ src/components/Landing/MintTattoo.jsx
@@ -0,0 +1,190 @@
+import React, { useState } from "react";
+import FlipCountdown from "@rumess/react-flip-countdown";
+import calculator from "../../assets/image/calculator.png";
+import { motion } from "framer-motion/dist/framer-motion";
+import "../../pages/css/MintTattoo.css";
+
+import mintIntroImg01 from "../../assets/image/tattoo_mint01.jpg";
+import mintIntroImg02 from "../../assets/image/tattoo_mint02.jpg";
+
+const MintTattoo = () => {
+  const [nft, setNft] = useState(1);
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  const plus_nft = (e_nft) => {
+    var p_nft = e_nft + 1;
+    if (p_nft >= 9) {
+      setTotalPrice(0);
+      setNft(9);
+    } else {
+      setTotalPrice(p_nft * 0);
+      setNft(p_nft);
+    }
+  };
+  const minus_nft = (e_nft) => {
+    var p_nft = e_nft - 1;
+    if (p_nft < 1) {
+      setTotalPrice(0);
+      setNft(1);
+    } else {
+      setTotalPrice(p_nft * 0);
+      setNft(p_nft);
+    }
+  };
+  const max_nft = () => {
+    setTotalPrice(0);
+    setNft(9);
+  };
+  return (
+    <div className="container">
+      <h2 className="text-uppercase MintIntroText1">The Last 1000 Days</h2>
+      <div className="minttatoos-container d-flex align-items-center justify-content-center">
+        <div className="PageMintTattoo align-items-center">
+          <div className="MintIntroImg" >
+            <img src={mintIntroImg01} alt="mint-intro-img01" />
+          </div>
+
+          <div className="MintMainSection d-flex flex-column justify-content-center align-items-center">
+
+
+            <div className="MintLine"></div>
+
+            <h3 className="text-uppercase MintIntroText2">
+              Booking<br></br>Under 5 Days
+            </h3>
+            <motion.div
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ type: "spring", stiffness: 50 }}
+              viewport={{ once: true, amount: 0 }}
+              className="count_panel MintCountPanel d-flex align-items-center font-bold"
+            >
+              <div className="mint_space"></div>
+              <div className="d-flex flex-column text-white justify-content-center align-items-center price_area MintPricePanel">
+                <div>Price</div>
+                <div>
+                  <span className="fs-30 text-pink totalprice">
+                    {totalPrice}
+                  </span>{" "}
+                  ETH
+                </div>
+              </div>
+              <div className="d-flex justify-content-end align-items-center minus_btn">
+                <span
+                  className="fs-20 text-white cursor-pointer"
+                  onClick={() => minus_nft(nft)}
+                >
+                  <i className="fa fa-minus" />
+                </span>
+              </div>
+              <div className="d-flex justify-content-center align-items-center flex-column cnt">
+                <input type="text" value={nft} disabled />
+              </div>
+              <div className="d-flex align-items-center plus_btn">
+                <span
+                  className="fs-20 text-white cursor-pointer"
+                  onClick={() => plus_nft(nft)}
+                >
+                  <i className="fa fa-plus" />
+                </span>
+              </div>
+              <div className="d-flex align-items-center justify-content-center max_area">
+                <span
+                  className="d-flex align-items-center justify-content-center fs-20 cursor-pointer max_btn"
+                  onClick={max_nft}
+                >
+                  max
+                </span>
+              </div>
+              <div className="mint_space"></div>
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ type: "spring", stiffness: 50 }}
+              viewport={{ once: true, amount: 0 }}
+              className="mint_btn MintSoonBtn"
+            >
+              <span className="fs-30 ls-4 text-white font-bold MintSoonBtnText">
+                MINT
+              </span>
+            </motion.div>
+          </div>
+        </div>
+        <div className="PageMintTattoo align-items-center">
+          <div className="MintIntroImg" >
+            <img src={mintIntroImg02} alt="mint-intro-img01" />
+          </div>
+
+          <div className="MintMainSection d-flex flex-column justify-content-center align-items-center">
+
+            <div className="MintLine"></div>
+
+            <h3 className="text-uppercase MintIntroText2">
+              Booking<br></br>Under 5 Days
+            </h3>
+            <motion.div
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ type: "spring", stiffness: 50 }}
+              viewport={{ once: true, amount: 0 }}
+              className="count_panel MintCountPanel d-flex align-items-center font-bold"
+            >
+              <div className="mint_space"></div>
+              <div className="d-flex flex-column text-white justify-content-center align-items-center price_area MintPricePanel">
+                <div>Price</div>
+                <div>
+                  <span className="fs-30 text-pink totalprice">
+                    {totalPrice}
+                  </span>{" "}
+                  ETH
+                </div>
+              </div>
+              <div className="d-flex justify-content-end align-items-center minus_btn">
+                <span
+                  className="fs-20 text-white cursor-pointer"
+                  onClick={() => minus_nft(nft)}
+                >
+                  <i className="fa fa-minus" />
+                </span>
+              </div>
+              <div className="d-flex justify-content-center align-items-center flex-column cnt">
+                <input type="text" value={nft} disabled />
+              </div>
+              <div className="d-flex align-items-center plus_btn">
+                <span
+                  className="fs-20 text-white cursor-pointer"
+                  onClick={() => plus_nft(nft)}
+                >
+                  <i className="fa fa-plus" />
+                </span>
+              </div>
+              <div className="d-flex align-items-center justify-content-center max_area">
+                <span
+                  className="d-flex align-items-center justify-content-center fs-20 cursor-pointer max_btn"
+                  onClick={max_nft}
+                >
+                  max
+                </span>
+              </div>
+              <div className="mint_space"></div>
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ type: "spring", stiffness: 50 }}
+              viewport={{ once: true, amount: 0 }}
+              className="mint_btn MintSoonBtn"
+            >
+              <span className="fs-30 ls-4 text-white font-bold MintSoonBtnText">
+                MINT
+              </span>
+            </motion.div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MintTattoo;
--- src/pages/js/tattoomint.js
+++ src/pages/js/tattoomint.js
@@ -5,76 +5,75 @@ import Welcome from "../../components/Landing/welcome";
 import Footer from "../../components/Landing/footer";
 import Web3 from "web3";
 import "../css/landing.css";
-import MintTattoo from "../../components/Landing/MintTattoo";
+import MintTattoo from "../../components/Landing/MintTattoo.jsx";
 
 const Tattoomint = () => {
-    const [metamaskConnected, setMetamaskConnnected] = useState(false);
-    const [account, setAccount] = useState();
-    const [networkId, setNetworkId] = useState();
-    const [isMetamask, setIsMetamask] = useState(true);
+  const [metamaskConnected, setMetamaskConnnected] = useState(false);
+  const [account, setAccount] = useState();
+  const [networkId, setNetworkId] = useState();
+  const [isMetamask, setIsMetamask] = useState(true);
 
-    useEffect(async () => {
-        await loadWeb3().then((data) => {
-            if (data !== false) {
-                loadBlockchainData();
-            }
-        });
-    }, []);
+  useEffect(async () => {
+    await loadWeb3().then((data) => {
+      if (data !== false) {
+        loadBlockchainData();
+      }
+    });
+  }, []);
 
-    const loadWeb3 = async () => {
-        if (window.ethereum) {
-            window.web3 = new Web3(window.ethereum);
-        } else if (window.web3) {
-            window.web3 = new Web3(window.web3.currentProvider);
-        } else {
-            setIsMetamask(false);
-            return false;
-        }
-    };
+  const loadWeb3 = async () => {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      setIsMetamask(false);
+      return false;
+    }
+  };
 
-    const loadBlockchainData = async () => {
-        const web3 = window.web3;
-        const accounts = await web3.eth.getAccounts();
-        const networkId = await web3.eth.net.getId();
-        setNetworkId(networkId);
-        if (accounts.length === 0) {
-            setMetamaskConnnected(false);
-        } else {
-            setMetamaskConnnected(true);
-            setAccount(accounts[0]);
-        }
+  const loadBlockchainData = async () => {
+    const web3 = window.web3;
+    const accounts = await web3.eth.getAccounts();
+    const networkId = await web3.eth.net.getId();
+    setNetworkId(networkId);
+    if (accounts.length === 0) {
+      setMetamaskConnnected(false);
+    } else {
+      setMetamaskConnnected(true);
+      setAccount(accounts[0]);
+    }
 
-        window.ethereum.on("accountsChanged", (accounts) => {
-            if (accounts.length > 0) setAccount(accounts[0]);
-            else setAccount();
-        });
-        window.ethereum.on("networkChanged", (networkId) => {
-            setNetworkId(networkId);
-        });
-    };
+    window.ethereum.on("accountsChanged", (accounts) => {
+      if (accounts.length > 0) setAccount(accounts[0]);
+      else setAccount();
+    });
+    window.ethereum.on("networkChanged", (networkId) => {
+      setNetworkId(networkId);
+    });
+  };
 
-    return (
-        <Fragment>
-            <MetamaskError
-                networkId={networkId}
-                metamaskConnected={metamaskConnected}
-                isMetamask={isMetamask}
-            />
-            <div>
-                <Header
-                    networkId={networkId}
-                    isMetamask={isMetamask}
-                    metamaskConnected={metamaskConnected}
-                    setMetamaskConnnected={setMetamaskConnnected}
-                    account={account}
-                />
-                <div className="container d-flex  align-items-center justify-content-center">
-                    <MintTattoo />
-                </div>
-                <Footer />
-            </div>
-        </Fragment>
-    );
+  return (
+    <Fragment>
+      <MetamaskError
+        networkId={networkId}
+        metamaskConnected={metamaskConnected}
+        isMetamask={isMetamask}
+      />
+      <div>
+        <Header
+          networkId={networkId}
+          isMetamask={isMetamask}
+          metamaskConnected={metamaskConnected}
+          setMetamaskConnnected={setMetamaskConnnected}
+          account={account}
+        />
+        
+        <MintTattoo />
+        <Footer />
+      </div>
+    </Fragment>
+  );
 };
 
 export default Tattoomint;

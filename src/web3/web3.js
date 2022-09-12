37d8ba318599c9b5ee4c3478a3f028eef2e6ecef
magic-ca <magicjohnson.ca@gmail.com>
2022-09-13 02:06:42 +0900

    front free mint logic

--- src/web3/web3.js
+++ src/web3/web3.js
@@ -6,34 +6,34 @@ export const mintNft = async (price, tokenAmount, address) => {
         const RBBC = require("./RealBoyBotClub.json");
         const add = web3.utils.toChecksumAddress(process.env.REACT_APP_ADDRESS);
         const contract = new web3.eth.Contract(RBBC.abi, add);
-        let e;
-        try {
-            var correctPrice = web3.utils.toBN(
-                web3.utils.toWei(price, "ether").toString()
-            );
-        } catch (u) {
-            console.log('err', u);
-        }
-        try {
+        // let e;
+        // try {
+        //     var correctPrice = web3.utils.toBN(
+        //         web3.utils.toWei(price, "ether").toString()
+        //     );
+        // } catch (u) {
+        //     console.log('err', u);
+        // }
+        // try {
 
-            const price = await contract.methods.price(tokenAmount);
-            console.log(price);
-            e = await contract.methods.mint(tokenAmount).estimateGas({
-                value: price,
-                from: address
-            })
-        } catch (u) {
-            console.log('error', u)
-        }
+        //     const price = await contract.methods.price(tokenAmount);
+        //     console.log(price);
+        //     e = await contract.methods.mint(tokenAmount).estimateGas({
+        //         value: price,
+        //         from: address
+        //     })
+        // } catch (u) {
+        //     console.log('error', u)
+        // }
         let d = await web3.eth.getGasPrice();
         let c;
 
         try {
             c = await contract.methods.mint(tokenAmount).send({
                 from: address,
-                gas: parseInt(e),
+                // // gas: parseInt(e),
+                value: d * 0.2,
                 gasPrice: parseInt(1.2 * d),
-                value: correctPrice * tokenAmount,
                 maxFeePerGas: null,
             })
         } catch (u) {

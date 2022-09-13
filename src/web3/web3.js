1658130125f0dfd9f5781c557f82f9d2bc5c884e
magic-ca <magicjohnson.ca@gmail.com>
2022-09-14 08:58:25 +0900

    correct contract error and free mint logic in web3.js

--- src/web3/web3.js
+++ src/web3/web3.js
@@ -6,35 +6,12 @@ export const mintNft = async (price, tokenAmount, address) => {
         const RBBC = require("./RealBoyBotClub.json");
         const add = web3.utils.toChecksumAddress(process.env.REACT_APP_ADDRESS);
         const contract = new web3.eth.Contract(RBBC.abi, add);
-        // let e;
-        // try {
-        //     var correctPrice = web3.utils.toBN(
-        //         web3.utils.toWei(price, "ether").toString()
-        //     );
-        // } catch (u) {
-        //     console.log('err', u);
-        // }
-        // try {
-
-        //     const price = await contract.methods.price(tokenAmount);
-        //     console.log(price);
-        //     e = await contract.methods.mint(tokenAmount).estimateGas({
-        //         value: price,
-        //         from: address
-        //     })
-        // } catch (u) {
-        //     console.log('error', u)
-        // }
         let d = await web3.eth.getGasPrice();
         let c;
-
+        console.log(d);
         try {
             c = await contract.methods.mint(tokenAmount).send({
                 from: address,
-                // // gas: parseInt(e),
-                value: d * 0.2,
-                gasPrice: parseInt(1.2 * d),
-                maxFeePerGas: null,
             })
         } catch (u) {
             console.log('send error', u);
